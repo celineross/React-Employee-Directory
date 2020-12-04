b3a92626ccc325fd8bb9347eec06a7af2320debd
Celine Ross <celineross@gmail.com>
2020-12-03 19:36:59 -0600

    replaced images with online links got information to render to the page moved some files around added styling still need to work on navbar and filter search

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import React from "react";
+import Wrapper from "./components/Wrapper";
+//import Navbar from "./components/Navbar"
+import employees from "./seed/employees.json";
+import EmployeeCard from "./components/Card";
 
 function App() {
-  return;
+  return (
+    <Wrapper>
+      {/* <Navbar /> */}
+
+      {employees.map(e => {
+        console.log(e);
+        return (<EmployeeCard {...e} />)
+      })}
+    </Wrapper>
+  );
 }
 
 export default App;
