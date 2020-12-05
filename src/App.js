382f7d464a67a305baac8aca5b07dc5d182e9b80
Celine Ross <celineross@gmail.com>
2020-12-04 18:34:41 -0600

    working on navbar and search functionality edited wrapper

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import React from "react";
 import Wrapper from "./components/Wrapper";
-//import Navbar from "./components/Navbar"
+import Navbar from "./components/Navbar"
 import employees from "./seed/employees.json";
 import EmployeeCard from "./components/Card";
 
 function App() {
   return (
-    <Wrapper>
-      {/* <Navbar /> */}
-
-      {employees.map(e => {
-        console.log(e);
-        return (<EmployeeCard {...e} />)
-      })}
-    </Wrapper>
+    <div>
+      <Navbar />
+      <Wrapper>
+        {employees.map(e => {
+          console.log(e);
+          return (<EmployeeCard {...e} />)
+        })}
+      </Wrapper>
+    </div>
   );
 }
 
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,8 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import "./style.css";
+import employees from "../../seed/employees.json";
 
-function Navbar(props) {
-    return;
+function Navbar() {
+    const [search, setSearch] = useState("");
+    const [filter, setFilter] = useState(false);
+    const [employees, setEmployees] = useState(employees);
+
+    function handleSearch(e) {
+        e.preventDefault();
+        setSearch(e.target.value);
+    }
+
+    function handleFilterName() {
+        if (!filter) {
+
+        }
+    }
+
+    // function handleFilterDepartment() {
+
+    // }
+
+    // function handleFilterRole() {
+
+    // }
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <ul className="navbar-nav">
+                <li className="nav-item">
+                    <form className="form-inline my-2 my-lg-0">
+                        <input
+                            className="form-control mr-sm-2"
+                            placeholder="Enter Name, Dept, or Role"
+                            aria-label="Search"
+                            type="text"
+                            value={search}
+                            onChange={handleSearch}
+                            required></input>
+                        <button onClick={handleFilterName}>Search by Name</button>
+                        {/* <button onClick={handleFilterDepartment}>Search by Department</button> */}
+                        {/* <button onClick={handleFilterRole}>Search by Role</button> */}
+                    </form>
+                </li>
+            </ul>
+        </nav>
+    );
 }
 
 export default Navbar;
